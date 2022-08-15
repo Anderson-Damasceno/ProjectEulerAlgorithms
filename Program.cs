e99258ca6f15e08a83bae2941fe76afc97284cb5
Anderson <andersondamascenodev@gmail.com>
2022-08-15 13:27:06 -0300

    Sum of Even Number in Fibonacci Sequence up to 4_000_000

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿using Problems.Sum_Of_Multiples_Of_3_And_5_Up_To_1000;
+﻿// using Problems.Sum_Of_Multiples_Of_3_And_5_Up_To_1000;
 
-Sum_Of_Multiples teste = new();
-teste.InitializingArray();
-Console.WriteLine(teste.SumOfMultiples());
+// Sum_Of_Multiples teste = new();
+// teste.InitializingArray();
+// Console.WriteLine(teste.SumOfMultiples());
 
+using Problems.Even_Fibonacci_Numbers;
 
+Sum_Of_Even_Terms teste = new();
+teste.UpTo4million();
+Console.WriteLine(teste.SumOfEven());
